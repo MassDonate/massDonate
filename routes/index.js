e9e0f92aa5e8b10c8e4fb930e550c4cd72ffeaaf
SocketPhys <SocketPhys@gmail.com>
2016-06-11 16:34:03 -0700

    AS

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var router = express.Router();
-
+const db = low('db.json')
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.post('/info', function(req, res, next) {
+  var name = req.body.name;
+  var phone = req.body.phone;
+  var address = req.body.address;
+  var email = req.body.email;
+  var fb = req.body.fb; 
+  db.defaults({ users: [] })
+  .value()
+  
+  db.get('users')
+  .push({ name: name, phone: phone, address: address, email: email, fb:fb})
+  .value()
+
+  var test = db.get('posts')
+  .find({ name:name })
+  .value()
+    
+  console.log(test)
+
   res.render('index');
 });
 
