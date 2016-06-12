11367e9d955c1dd87c3f2079172c106769999b36
Rishi Upadhyay <rishi7244@gmail.com>
2016-06-12 02:05:14 -0700

    minor changes

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
-app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
